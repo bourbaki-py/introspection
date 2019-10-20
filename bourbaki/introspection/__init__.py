89bf3f5a29dd4b1cd357f9aa5c19eeb48405f536
mattHawthorn <hawthorn.matthew@gmail.com>
2019-10-20 17:56:50 -0400

    Bump version: 0.2.0 → 0.2.1

--- bourbaki/introspection/__init__.py
+++ bourbaki/introspection/__init__.py
@@ -12,4 +12,4 @@ from .typechecking import type_checker
 from .types import issubclass_generic
 from .wrappers import ArgPreparer, cached_getter, lru_cache_sig_preserving, trace
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
