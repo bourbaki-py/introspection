f4ed07ef9b15c78bedab8fa75fcf36ddc9f872bd
mattHawthorn <hawthorn.matthew@gmail.com>
2019-11-17 11:08:32 -0500

    revert erroneous minor version bump

--- bourbaki/introspection/__init__.py
+++ bourbaki/introspection/__init__.py
@@ -12,4 +12,4 @@ from .typechecking import type_checker
 from .types import issubclass_generic
 from .wrappers import ArgPreparer, cached_getter, lru_cache_sig_preserving
 
-__version__ = '0.4.0'
+__version__ = '0.3.3'
